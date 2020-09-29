2530497f97dcb05f0c4b57c67fe04f233ce57115
Atul Raj <atulsid151@gmail.com>
2020-09-29 14:26:17 +0530

    add scroll

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,12 +1,74 @@
 import React from "react";
 import CollegeCard from "./CollegeCard";
+import data from "../colleges.json";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-function App() {
-  return (
-    <div className="App">
-      <CollegeCard />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      colleges: [],
+      index: 0,
+      hasMore: true,
+    };
+  }
+  componentDidMount() {
+    const { colleges } = data;
+    let newCollege = [colleges[0], colleges[1], colleges[2], colleges[3]];
+    console.log(newCollege);
+    this.setState({
+      colleges: newCollege,
+      index: 4,
+    });
+  }
+  loadmore = () => {
+    console.log("index", this.state.index);
+    if (this.state.index >= data.colleges.length - 1) {
+      this.setState({ hasMore: false });
+      return;
+    }
+
+    this.setState({
+      colleges: [
+        ...this.state.colleges,
+        this.state.index < data.colleges.length &&
+          data.colleges[this.state.index + 1],
+
+        this.state.index < data.colleges.length &&
+          data.colleges[this.state.index + 2],
+      ],
+      index: this.state.index + 2,
+    });
+  };
+
+  render() {
+    // const { colleges } = data;
+    console.log("this state changes", this.state.colleges);
+    return (
+      <div className="App">
+        <div className="main">
+          {/* {colleges.map((college, index) => (
+            <CollegeCard college={college} key={index} />
+          ))} */}
+          <InfiniteScroll
+            dataLength={this.state.colleges.length}
+            className="main"
+            next={this.loadmore}
+            hasMore={this.state.hasMore}
+          >
+            {this.state.colleges.length > 0
+              ? this.state.colleges.map(
+                  (college, index) =>
+                    college != undefined && (
+                      <CollegeCard college={college} key={index} />
+                    )
+                )
+              : null}
+          </InfiniteScroll>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Component/CollegeCard.js
+++ src/Component/CollegeCard.js
@@ -3,6 +3,8 @@ import collegeImage from "../assets/images/college_02.jpg";
 
 class CollegeCard extends Component {
   render() {
+    const { college } = this.props;
+
     return (
       <div className="card">
         <div className="promoted">PROMOTED</div>
@@ -11,21 +13,24 @@ class CollegeCard extends Component {
           <img className="card-header-img" src={collegeImage} />
           <div className="college-rating-container">
             <div className="college-rating-box">
-              <span>4.5/5</span>
+              <span>{college.rating}/5</span>
               <br />
-              <span className="rating-remark">Very Good</span>
+              <span className="rating-remark">{college.rating_remarks}</span>
             </div>
           </div>
           <div className="college-tags">
-            <p>best colleges in 2018</p>
-            <p>2kms away</p>
+            {/* <p>{college.tags[0]}</p>
+            <p>{college.tags[1]}</p> */}
+            {college.tags.map((tag, index) => (
+              <p key={`tag-${index}`}>{tag}</p>
+            ))}
           </div>
           <div className="ranking"></div>
         </div>
         <div className="card-footer">
           <div className="college-name-rate-wrapper">
             <div className="college-name">
-              <span>Hansraj College of Delhi University</span>
+              <span>{college.college_name}</span>
             </div>
             <div className="college-start-rating">
               <span className="fa fa-star checked"></span>
@@ -37,38 +42,66 @@ class CollegeCard extends Component {
           </div>
           <div className="college-location">
             <span className="college-nearby-places-1">
-              Near Vishwavidyalya Metro Station
+              {college.nearest_place[0]}
             </span>
             &nbsp;
             <span className="college-nearby-places-2">
-              2 Kms away from bus stand
+              {college.nearest_place[1]}{" "}
             </span>
           </div>
           <div className="college-famous-nearby-places">
             <span className="text-style1 text-style-2">93% Match</span> &nbsp;
-            <span className="text-style1">2.5km</span>
-            from GTB Nagar <span className="text-style1 ">7 Kms</span> from
-            Rajiv Chowk
+            <span className="text-style1">
+              {college.famous_nearest_places.split(" ")[0]}{" "}
+            </span>
+            {/* from GTB Nagar{" "} */}
+            {college.famous_nearest_places
+              .split(",")[0]
+              .split(" ")
+              .slice(1)
+              .join(" ")}
+            <span className="text-style1 ">
+              {college.famous_nearest_places.split(",")[1].split(" ")[1]}
+              {" km"}
+            </span>{" "}
+            {college.famous_nearest_places
+              .split(",")[1]
+              .split(" ")
+              .slice(3)
+              .join(" ")}
           </div>
           <div className="offer-container">
             <div className="college-offer-text">
               <p>
-                Flat Rs<span className="text-style1 text-style-2">2000</span>{" "}
+                Flat Rs
+                <span className="text-style1 text-style-2">
+                  {college.offertext.split("+")[0].split(" ")[1].slice(2)}
+                </span>
                 off + upto Rs
-                <span className="text-style1 text-style-2 ">500</span>wallet! to
-                avail... <span className="text-style-3">Login</span>
+                <span className="text-style1 text-style-2 ">
+                  {" "}
+                  {college.offertext.split("+")[1].split(" ")[3]}
+                </span>
+                wallet! to avail... <span className="text-style-3">Login</span>
               </p>
             </div>
             <div className="college-amenties">
-              <span>Free Cancellation</span>,&nbsp;
-              <span>Free Wifi</span>
+              {/* <span>Free Cancellation</span>,&nbsp;
+              <span>Free Wifi</span> */}
+              {college.amenties.map((item, index) => (
+                <span key={`amenty-${index}`}>{". " + item}</span>
+              ))}
             </div>
           </div>
           <div className="college-fee">
-            <span className="college-orignal-fee">₹6,875</span>
-            <span className="college-discount-percent">20</span>
-            <p className="college-discounted-fee">₹ 5,768</p>
-            <span className="fee-cycle">Per Semester (3months)</span>
+            <span className="college-orignal-fee">
+              ₹{college.original_fees}
+            </span>
+            <span className="college-discount-percent">
+              <li>{college.discount}</li>
+            </span>
+            <p className="college-discounted-fee">₹{college.discounted_fees}</p>
+            <span className="fee-cycle">{college.fees_cycle}</span>
           </div>
         </div>
       </div>
