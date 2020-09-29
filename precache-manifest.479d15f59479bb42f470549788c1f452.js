2a2525df9e27d81f584f09f0516acd17d6ad33f0
Atul Raj <atulsid151@gmail.com>
2020-09-29 14:31:10 +0530

    Updates

---
+++ precache-manifest.479d15f59479bb42f470549788c1f452.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33eb31b2f9096263de8ef4ef4bc0d068",
+    "url": "/CollegeDunia/index.html"
+  },
+  {
+    "revision": "b66739b42beea2db1af6",
+    "url": "/CollegeDunia/static/css/main.5abdcf25.chunk.css"
+  },
+  {
+    "revision": "757e60751f38d0d696b7",
+    "url": "/CollegeDunia/static/js/2.3fc89f4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CollegeDunia/static/js/2.3fc89f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b66739b42beea2db1af6",
+    "url": "/CollegeDunia/static/js/main.fd7f79d3.chunk.js"
+  },
+  {
+    "revision": "151b4ea2e7480505020e",
+    "url": "/CollegeDunia/static/js/runtime-main.8ec512db.js"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/CollegeDunia/static/media/college_02.619ee97e.jpg"
+  }
+]);
